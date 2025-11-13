2287d423e198ea06d773637fec5252e613c61481
Modovisa <so@modovisa.com>
2025-11-13 21:04:48 +0530

    UpgradePlanModal.tsx slider-4

--- src/components/profile/UpgradePlanModal.tsx
+++ src/components/profile/UpgradePlanModal.tsx
@@ -105,7 +105,7 @@ export default function UpgradePlanModal({ open, onClose, tiers, currentPlanAmou
                   step={1}
                   value={idx}
                   onChange={(e) => setEvents(SNAP_STEPS[parseInt(e.target.value, 10)])}
-                  className="mv-range absolute inset-[2px_0_0] w-full range-primary"
+                  className="mv-range absolute inset-[4px_0_0] w-full range-primary"
                 />
               </div>
 
